8de1a3f815f67a0ff331eaf4a7e8b4ac33f4c4fa
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-08 14:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //your JS code here. If required.
- const sizeElement = document.createElement("h1");
- sizeElement.innerText = `Width: ${window.innerWidth} and Height ${window.innerHeight}` ;
-
-
-document.body.append(sizeElement);
-
- window.onresize = function(){
-      console.log(window.innerHeight, window.innerWidth);
-     sizeElement.innerText = `Width: ${window.innerWidth} and Height ${window.innerHeight}` ;
- }
+  let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+ 
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
